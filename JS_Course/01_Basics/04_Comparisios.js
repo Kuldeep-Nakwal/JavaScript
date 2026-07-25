bd0881e82256030c93f57b8c9becdfe521cb4a5e
Kuldeep Nakwal <knakwal@gmail.com>
2026-07-25 21:28:32 +0000

    Comparisions

---
+++ JS_Course/01_Basics/04_Comparisios.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+/*
+Avoid following type of comparision with different datatypes it may be confusing.
+Always focus on writing clean code.
+*/
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
