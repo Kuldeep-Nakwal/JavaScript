76fa501b5bced2fdba81ca7803a0ec58327be2bc
Kuldeep Nakwal <knakwal@gmail.com>
2026-07-25 20:33:54 +0000

    Datatypes

---
+++ JS_Course/01_Basics/02_Datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // Treat JavaScript code newer version
+
+// alert(3 +3); // We are using NodeJS not browser so in a different way we can flash the alert message.
+
+console.log(3 + 
+    3);  // Code Readability should be high so it is not recommanded
+
+console.log(3 + 3); console.log("Kuldeep"); // Not recommanded due to code readability
+
+let name = "Kuldeep";
+let age = 37;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
