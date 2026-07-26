fd3d5d6af54cf2cfff830cbb346183103956b66c
Kuldeep Nakwal <knakwal@gmail.com>
2026-07-26 19:51:29 +0000

    Truthy Falsy edited

--- JS_Course/01_Basics/07_TruthyAndFalsy.js
+++ JS_Course/01_Basics/07_TruthyAndFalsy.js
@@ -7,7 +7,68 @@
 */
 
 /*
-FALSY values: 0, 0-, 0n(BigInt value), ""(Empty String), null, undefined, NaN
+FALSY values: 0, 0n(BigInt value), ""(Empty String), null, undefined, NaN
 
 TRUTHY values: Everything Else
 */
+
+if(0){
+    console.log("It is True value")
+} else{
+    console.log("It is a Falsy value")
+}
+
+if(0n){
+    console.log("It is True value")
+} else{
+    console.log("It is a Falsy value")
+}
+
+if(""){
+    console.log("It is True value")
+} else{
+    console.log("It is a Falsy value")
+}
+
+if(null){
+    console.log("It is True value")
+} else{
+    console.log("It is a Falsy value")
+}
+
+if(undefined){
+    console.log("It is True value")
+} else{
+    console.log("It is a Falsy value")
+}
+
+if(NaN){
+    console.log("It is True value")
+} else{
+    console.log("It is a Falsy value")
+}
+
+if(1){
+    console.log("It is True value")
+} else{
+    console.log("It is a Falsy value")
+}
+
+if(23){
+    console.log("It is True value")
+} else{
+    console.log("It is a Falsy value")
+}
+
+if(" "){
+    console.log("It is True value")
+} else{
+    console.log("It is a Falsy value")
+}
+
+if("Kuldeep"){
+    console.log("It is True value")
+} else{
+    console.log("It is a Falsy value")
+}
+
