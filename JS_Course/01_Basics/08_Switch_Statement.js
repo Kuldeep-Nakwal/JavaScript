a2e85656789ce1c7db979dead78ed5fdc29c25fd
Kuldeep Nakwal <knakwal@gmail.com>
2026-07-26 20:04:22 +0000

    Switch Statement added

---
+++ JS_Course/01_Basics/08_Switch_Statement.js
@@ -0,0 +1,18 @@
+let color = "blue";
+
+switch (color) {
+    case "red":
+        console.log("Stop")
+        break;
+    case "yellow":
+        console.log("Slow down")
+        break;
+    case "green":
+        console.log("Go")
+        break;
+    default:
+        console.log("Traffic Light is Broken.")
+        break;
+}
+
+console.log("After the Switch Statement.")
