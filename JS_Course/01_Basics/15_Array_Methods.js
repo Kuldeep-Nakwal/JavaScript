6742c093d2581eb3eca38d02741cbfcbf7279f62
Kuldeep Nakwal <knakwal@gmail.com>
2026-07-29 20:03:34 +0000

    Array Methods Push Pop

---
+++ JS_Course/01_Basics/15_Array_Methods.js
@@ -0,0 +1,22 @@
+// ***************************** Array Methods ***************************************
+
+// ****************************** PUSH and POP ***************************************
+
+let cars = ["Maruti", "Hyundai", "Honda"];
+
+cars.push("Toyota");
+
+console.log(cars);
+
+console.log(cars.length);
+
+console.log(cars.push("BMW"));
+
+console.log(cars);
+
+cars.pop();
+
+console.log(cars);
+
+
+
