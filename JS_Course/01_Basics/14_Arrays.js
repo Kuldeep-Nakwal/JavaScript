f1ea3851eddf3e390039f5b46889018c47e55da4
Kuldeep Nakwal <knakwal@gmail.com>
2026-07-29 19:37:00 +0000

    Arrays

---
+++ JS_Course/01_Basics/14_Arrays.js
@@ -0,0 +1,19 @@
+// ********************************** ARRAYS *******************************************
+
+// Arrays are mutable in nature.
+
+let arr = ["Kuldeep", "Divyansh", "Gaurav", 23, 77.3];
+
+console.log(arr);
+
+arr[1] = "Deepak";
+
+console.log(arr);
+
+console.log(arr[0][1]);
+
+arr[9] = 23;
+ 
+console.log(arr);
+
+
