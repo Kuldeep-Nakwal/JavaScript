e02c02099234cd1d05c1a46a4d28ed68a1a405ff
Kuldeep Nakwal <knakwal@gmail.com>
2026-07-29 19:14:53 +0000

    Replace and Repeat added.

---
+++ JS_Course/01_Basics/13_Replace_Repeat.js
@@ -0,0 +1,35 @@
+// ********************************* REPLACE ******************************************
+
+// Searches a value in the string and returns a new string with the value replaced.
+
+let str = "I love Coding.";
+
+let newStr = str.replace("love", "do");
+
+console.log(newStr);
+
+// Replaces only first occurance of the element not all elements are replaced.
+
+let newStr1 = str.replace("o", "a");
+
+console.log(newStr1);
+console.log(str);
+
+let msg = "KuldeepKuldeep"
+
+let newMsg = msg.replace("Kuldeep", "Nakwal");
+
+console.log(newMsg);
+
+
+// *********************************  REPEAT  ******************************************
+
+// Returns a string with a number of copies of a string.
+
+let item = "Mango ";
+
+console.log(item.repeat(3));
+
+console.log(item);
+
+
