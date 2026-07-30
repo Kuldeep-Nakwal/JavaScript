38667d560c09692804156c47d575947b8cbdc509
Kuldeep Nakwal <knakwal@gmail.com>
2026-07-30 08:05:26 +0000

    Conncatenation and Reverse added

---
+++ JS_Course/01_Basics/17_Concatenation_Reverse.js
@@ -0,0 +1,20 @@
+// ************************** CONCATENATION ************************************
+
+let primary = ["red", "blue", "yellow"];
+let secondary = ["orange", "pink", "violet"];
+
+primary.concat(secondary);
+
+console.log(primary);
+console.log(primary.concat(secondary));
+
+secondary.concat(primary);
+console.log(secondary.concat(primary));
+
+// ****************************** REVERSE **************************************
+
+primary.reverse();
+
+console.log(primary);
+console.log(primary.reverse());
+
