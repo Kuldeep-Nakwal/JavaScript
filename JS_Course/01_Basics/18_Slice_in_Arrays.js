28f9805b9bb66284d5cbad6216fdb81773912229
Kuldeep Nakwal <knakwal@gmail.com>
2026-07-30 08:27:08 +0000

    Slice method in arrays is added

---
+++ JS_Course/01_Basics/18_Slice_in_Arrays.js
@@ -0,0 +1,10 @@
+// ****************************** SLICE ****************************************
+
+let colors = ["red","yellow","green","orange","violet","white"];
+
+console.log(colors.slice());
+console.log(colors.slice(3));
+console.log(colors.slice(2,5));
+console.log(colors.slice(-2));
+
+
