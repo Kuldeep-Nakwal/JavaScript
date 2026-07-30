e9ccbfbc90c683b83c8191aeaf20530a99cedc1c
Kuldeep Nakwal <knakwal@gmail.com>
2026-07-30 07:50:23 +0000

    indexOf and includes added

---
+++ JS_Course/01_Basics/16_indexOf_includes.js
@@ -0,0 +1,12 @@
+// ******************************** indexOf ************************************
+
+let cars = ["bmw","maruti","honda"];
+
+console.log(cars.indexOf("bmw"));
+console.log(cars.indexOf("honda"));
+console.log(cars.indexOf("kia"));
+
+// ****************************** includes *************************************
+
+console.log(cars.includes("bmw"));
+console.log(cars.includes("ferrari"));
