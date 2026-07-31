7d418c2de8c07bdf06dc9601ef97bd58da208749
Kuldeep Nakwal <knakwal@gmail.com>
2026-07-31 03:36:50 +0000

    Sort Method only for String type elements

---
+++ JS_Course/01_Basics/20_Sort_Method.js
@@ -0,0 +1,21 @@
+// ********************************** SORT METHOD **************************************
+
+// Only sort string type array elements in ascending order.
+
+let cars = ["toyota,", "gwagon", "maruti", "ferrari", "bmw", "hyundai", "mahindra", "tata"];
+
+let sortedCars = cars.sort();
+
+console.log(sortedCars);
+
+
+// But not applicable for Numbers see example:
+
+let nums = [23, 58, 412, 94, 38, 43, 27, 204];
+
+let sortedNums = nums.sort();
+
+console.log(sortedNums);
+
+
+
